978dd0eaa9dd8c2293f28d1d57a05266afe7a086
anuzza <anuja3580@gmail.com>
2022-09-10 11:08:06 -0500

    merged to dev

--- src/App.js
+++ src/App.js
@@ -13,27 +13,19 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
+          <Route path="/" to="/landing" exact component={LandingPage} />
           <Route
             path="/home"
             exact
             render={(routerProps) => {
               return <HomePage routerProps={routerProps} />;
             }}
-        <Route path="/" to="/landing" exact component={LandingPage} />
-          <Route path="/home" exact render={(routerProps) => {
-            return (
-              <HomePage
-                routerProps={routerProps} />
-            );
-          }}
           />
           <Route path="/landing" exact component={LandingPage} />
           <Route path="/communal" exact component={CommunalPage} />
           <Route path="/food" exact component={FoodPage} />
           <Route path="/energy" exact component={EnergyPage} />
-          <Route path="/recyclables" exact component={RecyclablePage} />
           <Route path="/recyclable" exact component={RecyclablePage} />
-
         </Switch>
       </BrowserRouter>
     </div>
