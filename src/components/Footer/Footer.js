111f72a5fb62987ee8713a10c7d10ff47444c7b2
macapedi <macapedi90@gmail.com>
2022-09-09 23:31:46 -0400

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+
+import "./Footer.scss";
+
+
+
+function Footer() {
+
+
+
+
+
+
+    return (
+        <footer className="footer">
+           
+
+
+        </footer>
+    );
+}
+export default Footer;
