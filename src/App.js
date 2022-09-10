7957f2df326250a419d560cb4c862a4c26ec3c69
anuzza <anuja3580@gmail.com>
2022-09-10 00:06:44 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <h1>Sustaineers</h1>
     </div>
   );
 }
