af8cb0a45746807a8f9374b3313a09b6fe2da8f8
macapedi <macapedi90@gmail.com>
2022-09-10 01:49:58 -0400

    routing and pages creation done

---
+++ src/pages/RecyclablePage/RecyclablesPage.js
@@ -0,0 +1,11 @@
+import './RecyclablePage.scss';
+
+function RecyclablePage() {
+  return (
+    <div>
+     <h1>This is Recyclable Page</h1>
+    </div>
+  );
+}
+
+export default RecyclablePage;
---
+++ src/pages/SharePage/SharePage.js
@@ -0,0 +1,11 @@
+import './SharePage.scss';
+
+function SharePage() {
+  return (
+    <div>
+     <h1>This is Share Page</h1>
+    </div>
+  );
+}
+
+export default SharePage;
