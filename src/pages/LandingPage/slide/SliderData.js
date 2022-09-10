4e6d1a1929d6ad5b807a6e9bc23413e4eabb5c56
anuzza <anuja3580@gmail.com>
2022-09-10 12:42:45 -0500

    worked on landing page

---
+++ src/pages/LandingPage/slide/SliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+  {
+    image: "https://static.toiimg.com/photo/73325395.cms",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1611735341450-74d61e660ad2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHNhdmUlMjBlYXJ0aHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60https://images.unsplash.com/photo-1611735341450-74d61e660ad2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHNhdmUlMjBlYXJ0aHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1611284446314-60a58ac0deb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  },
+  {
+    image:
+      "https://www.bathingsolutions.co.uk/wp-content/uploads/2021/05/save-water-bathroom.jpg",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1616745207210-a98414926a3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZW5lcmd5JTIwY29uc2VydmF0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2F2ZSUyMHRyZWVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
