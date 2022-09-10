44a5016e427f144d2fdced09e6720baa46c29e27
macapedi <macapedi90@gmail.com>
2022-09-10 14:57:06 -0400

    star icon added to community page

--- src/pages/CommunityPage/CommunityPage.js
+++ src/pages/CommunityPage/CommunityPage.js
@@ -1,9 +1,12 @@
 import './CommunityPage.scss';
+import { AiTwotoneStar } from "react-icons/ai";
 
 function CommunityPage() {
   return (
     <div>
      <h1>This is Community Page</h1>
+     <AiTwotoneStar className='star-logo'/>
+   
     </div>
   );
 }
