3c3aa0a61b2faa95e88735ba1214ec685ba40ae1
macapedi <macapedi90@gmail.com>
2022-09-10 10:32:24 -0400

    mixings & variables added in partial styles folder

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.scss';
+import { Link, withRouter } from "react-router-dom";
+
+function Header() {
+    return (
+        <header className='header'>
+            
+
+        </header>
+    );
+}
+
+export default Header;
