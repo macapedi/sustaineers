6edbe8313f11d2e799f98dab0fe40898dcf5096f
medhug <95834183+medhug@users.noreply.github.com>
2022-09-10 20:51:44 -0400

    add one checkbox

--- src/pages/FoodPage/FoodPage.js
+++ src/pages/FoodPage/FoodPage.js
@@ -1,9 +1,23 @@
-import './FoodPage.scss';
+import "./FoodPage.scss";
 
-function FoodPage() {
+function FoodPage(props) {
   return (
-    <div>
-     <h1>This is Food Page</h1>
+    <div className="food">
+      <div className="food-title">
+        <h1>This is Food Page</h1>
+      </div>
+      <div className="food-category">
+        <h2>Eating Local</h2>
+        <form>
+          <div>
+            <input type="checkbox" id="localFarmersMarket" name="local" value="1" />
+            <label htmlFor="localFarmersMarket">Bought from farmer's market?</label>
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
