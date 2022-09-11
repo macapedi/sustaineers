1119538a41b78dc56aa8138f15cfe63966de0ee7
anuzza <anuja3580@gmail.com>
2022-09-10 22:49:10 -0500

    added recycle and energy page

--- src/pages/EnergyPage/EnergyPage.js
+++ src/pages/EnergyPage/EnergyPage.js
@@ -1,9 +1,110 @@
-import './EnergyPage.scss';
+import "./EnergyPage.scss";
+import Category from "../../components/CategoryCard/Category";
+import Footer from "../../components/Footer/Footer";
 
 function EnergyPage() {
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-     <h1>This is Energy Page</h1>
+      <div>
+        <h1>This is Energy Page</h1>
+      </div>
+      <Category>
+        <div className="food-category__subcategory">
+          <h2>Electricity</h2>
+        </div>
+
+        <form className="food-category__form" onSubmit={onSubmitHandler}>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="energy" name="energy" value="100" />
+            <label htmlFor="energy" />
+            Turned off unnecessary lights?
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="metal" name="energy" value="100" />
+            <label htmlFor="metal">Minimized time used For electronics?</label>
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="utensils" name="energy" value="100" />
+            <label htmlFor="utensils">
+              Turned off AC and lights when not home?
+            </label>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </Category>
+
+      <Category>
+        <div className="food-category__subcategory">
+          <h2>Water</h2>
+        </div>
+
+        <form className="food-category__form" onSubmit={onSubmitHandler}>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="energy" name="energy" value="100" />
+            <label htmlFor="energy" />
+            Used your automatic dishwasher/wachine machines for full loads only?
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="metal" name="energy" value="100" />
+            <label htmlFor="metal">Checked faucets and pipes for leaks?</label>
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="utensils" name="energy" value="100" />
+            <label htmlFor="utensils">Took shorter showers?</label>
+          </div>
+
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="solo" name="energy" value="100" />
+            <label htmlFor="solo">
+              Turned off water while brushing/shaving?
+            </label>
+          </div>
+
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="solo" name="energy" value="100" />
+            <label htmlFor="solo">Watered your lawn only when it needs?</label>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </Category>
+      <Category>
+        <div className="food-category__subcategory">
+          <h2>Gas</h2>
+        </div>
+
+        <form className="food-category__form" onSubmit={onSubmitHandler}>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="energy" name="energy" value="100" />
+            <label htmlFor="energy" />
+            Drove electric vehicles?
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="metal" name="energy" value="100" />
+            <label htmlFor="metal">Combined short trips while driving?</label>
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="utensils" name="energy" value="100" />
+            <label htmlFor="utensils">Inflated your tires?</label>
+          </div>
+
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="utensils" name="energy" value="100" />
+            <label htmlFor="utensils">Replaced your air filter?</label>
+          </div>
+
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="solo" name="energy" value="100" />
+            <label htmlFor="solo">Used the highest feasible gear?</label>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </Category>
     </div>
   );
 }
--- src/pages/RecyclablePage/RecyclablesPage.js
+++ src/pages/RecyclablePage/RecyclablesPage.js
@@ -1,9 +1,107 @@
-import './RecyclablePage.scss';
+import "./RecyclablePage.scss";
+import Category from "../../components/CategoryCard/Category";
 
 function RecyclablePage() {
+  const onSubmitHandler = (e) => {
+    e.preventDeafault();
+  };
+
   return (
     <div>
-     <h1>This is Recyclable Page</h1>
+      <div>
+        <h1>This is Recyclable Page</h1>
+      </div>
+      <Category>
+        <div className="food-category__subcategory">
+          <h2>Reduce Plastic Use</h2>
+        </div>
+
+        <form className="food-category__form" onSubmit={onSubmitHandler}>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="reusable" name="plastic" value="1" />
+            <label htmlFor="reusable" />
+            Used reusable Bags?
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="metal" name="plastic" value="1" />
+            <label htmlFor="metal">Not used plastic bags?</label>
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="utensils" name="plastic" value="1" />
+            <label htmlFor="utensils">
+              Used washable non-plastic utensils?
+            </label>
+          </div>
+
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="solo" name="plastic" value="1" />
+            <label htmlFor="solo">
+              Used ceramic instead of solo or styrofoam cups?
+            </label>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </Category>
+
+      <Category>
+        <div className="food-category__subcategory">
+          <h2>Clothing</h2>
+        </div>
+
+        <form className="food-category__form" onSubmit={onSubmitHandler}>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="reusable" name="plastic" value="1" />
+            <label htmlFor="reusable" />
+            Avoided fast fashion items?
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="metal" name="plastic" value="1" />
+            <label htmlFor="metal">Bought less items?</label>
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="utensils" name="plastic" value="1" />
+            <label htmlFor="utensils">
+              Chose higher quality materials that last longer?
+            </label>
+          </div>
+
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="solo" name="plastic" value="1" />
+            <label htmlFor="solo">Repaired or donated your clothes?</label>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </Category>
+
+      <Category>
+        <div className="food-category__subcategory">
+          <h2>Paper</h2>
+        </div>
+
+        <form className="food-category__form" onSubmit={onSubmitHandler}>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="reusable" name="plastic" value="1" />
+            <label htmlFor="reusable" />
+            Used electronic notes?
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="metal" name="plastic" value="1" />
+            <label htmlFor="metal">
+              Used washable towels instead of paper for cleaning?
+            </label>
+          </div>
+          <div className="food-category__form--boxes">
+            <input type="checkbox" id="utensils" name="plastic" value="1" />
+            <label htmlFor="utensils">
+              Printed double sides for school or work?{" "}
+            </label>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </Category>
     </div>
   );
 }
