b6eb826b438687bb6c91de1eab425a402429f1ce
medhug <95834183+medhug@users.noreply.github.com>
2022-09-10 21:32:02 -0400

    set up checkbox

--- src/pages/FoodPage/FoodPage.js
+++ src/pages/FoodPage/FoodPage.js
@@ -7,11 +7,38 @@ function FoodPage(props) {
         <h1>This is Food Page</h1>
       </div>
       <div className="food-category">
-        <h2>Eating Local</h2>
-        <form>
-          <div>
-            <input type="checkbox" id="localFarmersMarket" name="local" value="1" />
-            <label htmlFor="localFarmersMarket">Bought from farmer's market?</label>
+        <div className="food-category__subcategory">
+          <h2>Eating Local</h2>
+        </div>
+
+        <form className="food-category__form">
+          <div className="food-category__form--boxes">
+              <input
+                type="checkbox"
+                id="localFarmersMarket"
+                name="local"
+                value="1"
+              />
+              <label htmlFor="localFarmersMarket" />
+                Bought from farmer's market?
+          </div>
+          <div className="food-category__form--boxes">
+            <input
+              type="checkbox"
+              id="localPlantGarden"
+              name="local"
+              value="1"
+            />
+            <label htmlFor="localPlantGarden">Plant own produce?</label>
+          </div>
+          <div className="food-category__form--boxes">
+            <input
+              type="checkbox"
+              id="localjoinCoop"
+              name="local"
+              value="1"
+            />
+            <label htmlFor="localPlantGarden">Join a Co-op?</label>
           </div>
           <div>
             <button type="submit">Submit</button>
