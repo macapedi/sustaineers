092a5f2193e11cb62fdcc39424854e7a6693e716
macapedi <macapedi90@gmail.com>
2022-09-11 06:24:27 -0400

    profile page functional for each user

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,19 +1,31 @@
 import "./ProfilePage.scss";
 import React from "react";
-import userdata from "../../data/users.json";
+import userData from "../../data/users.json";
 import user1 from "../../assets/users/user1.jpg";
 import user2 from "../../assets/users/user2.jpg";
 import user3 from "../../assets/users/user3.jpg";
 import user4 from "../../assets/users/user4.jpg";
 
 class ProfilePage extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>This is Profile Page</h1>
-      </div>
-    );
-  }
+
+   
+
+    render() {
+      const  currentUser = this.props.routerProps.match.params.id;
+      const  userMatch = userData.find(user => user.user === currentUser);
+        console.log(this.props.routerProps.match.params.id);
+        console.log(userMatch);
+        if (userMatch) {
+            return (
+                <div>
+                    <h1>{userMatch.name}'s achievements</h1>
+                    <p></p>
+                </div>
+            )
+        } else {
+            console.log("not user found")
+        }
+    }
 }
 
 export default ProfilePage;
